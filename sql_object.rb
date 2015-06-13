60d92917aac6e97b0791b1cc29dae10da53fed91
Charles Zahn <charlesazahn@gmail.com>
2015-06-12 23:43:56 -0400

    Add SQLObject::columns class method.

--- sql_object.rb
+++ sql_object.rb
@@ -2,4 +2,14 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 
 class SQLObject
+  def self.columns
+    @columns ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+      .first
+      .map(&:to_sym)
+  end
 end
