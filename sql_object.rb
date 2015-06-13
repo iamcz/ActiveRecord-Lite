4bedbc403e6bb12844d28f2c407dcf710f2eff5d
Charles Zahn <charlesazahn@gmail.com>
2015-06-13 00:02:42 -0400

    Add SQLObject::table_name and ::table_name= class methods

--- sql_object.rb
+++ sql_object.rb
@@ -2,14 +2,24 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 
 class SQLObject
-  def self.columns
-    @columns ||= DBConnection.execute2(<<-SQL)
-      SELECT
-        *
-      FROM
-        #{self.table_name}
-    SQL
-      .first
-      .map(&:to_sym)
+  class < self
+    def columns
+      @columns ||= DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+      SQL
+        .first
+        .map(&:to_sym)
+    end
+
+    def table_name
+      @table_name ||= self.to_s.underscore.pluralize
+    end
+
+    def table_name=(table_name)
+      @table_name = table_name
+    end
   end
 end
